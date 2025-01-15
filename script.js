b83a7883909f0f2a75a2330a3faa47187f2bc13e
veian09 <20201282@nbsc.edu.ph>
2025-01-15 15:24:23 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const userDetails = {
+    name: "Veian Keth",
+    gender: "Female",
+    address: "Dalirig, MFB",
+    mobile: "123456789"
+}
+
+document.addEventListener("DOMContentLoaded",); {
+    document.getElementById("name").textContent = userDetails.name;
+
+}
